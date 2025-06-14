288fe309eaf08d846e760e84be86337740daf103
Rishabh Garcha <rish889@gmail.com>
2025-06-14 17:11:19 +0530

    Initial Commit

---
+++ src/main/java/com/rish889/rishSpringBoot3/_Java24Maven/Application.java
@@ -0,0 +1,13 @@
+package com.rish889.rishSpringBoot3._Java24Maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rish889/rishSpringBoot3/_Java24Maven/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rish889.rishSpringBoot3._Java24Maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
