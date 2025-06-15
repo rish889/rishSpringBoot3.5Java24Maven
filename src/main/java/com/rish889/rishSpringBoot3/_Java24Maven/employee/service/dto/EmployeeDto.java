41c3e14c5b077839c39a437362d9334902d77008
Rishabh Garcha <rish889@gmail.com>
2025-06-15 09:25:48 +0530

    Employee Service

--- src/main/java/com/rish889/rishSpringBoot3/_Java24Maven/employee/service/dto/EmployeeDto.java
+++ src/main/java/com/rish889/rishSpringBoot3/_Java24Maven/employee/service/dto/EmployeeDto.java
@@ -1,21 +1,4 @@
 package com.rish889.rishSpringBoot3._Java24Maven.employee.service.dto;
 
-public class EmployeeDto {
-    private String id;
-    private String name;
-
-    public EmployeeDto(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getId() {
-        return id;
-    }
+public record EmployeeDto(String id, String name) {
 }
-
-
