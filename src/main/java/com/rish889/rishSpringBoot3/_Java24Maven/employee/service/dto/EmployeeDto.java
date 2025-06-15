605aa06c0eac31f6e04eb1a03df70fe775bd54d2
Rishabh Garcha <rish889@gmail.com>
2025-06-15 10:30:42 +0530

    Employee Service

--- src/main/java/com/rish889/rishSpringBoot3/_Java24Maven/employee/service/dto/EmployeeDto.java
+++ src/main/java/com/rish889/rishSpringBoot3/_Java24Maven/employee/service/dto/EmployeeDto.java
@@ -2,12 +2,14 @@ package com.rish889.rishSpringBoot3._Java24Maven.employee.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class EmployeeDto {
     @JsonProperty("employee_id")
     private String employeeId;
