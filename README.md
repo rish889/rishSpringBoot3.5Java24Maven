8789344aa6bce7f2b3bf04886b504b567ada1564
Rishabh Garcha <rish889@gmail.com>
2025-06-15 09:48:52 +0530

    Employee Service

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rishSpringBoot3.5Java24Maven
 https://github.com/rish889/rishFeb2024/tree/main
+https://www.baeldung.com/spring-boot-clean-architecture
--- src/main/java/com/rish889/rishSpringBoot3/_Java24Maven/employee/service/controller/EmployeeController.java
+++ src/main/java/com/rish889/rishSpringBoot3/_Java24Maven/employee/service/controller/EmployeeController.java
@@ -1,15 +1,18 @@
 package com.rish889.rishSpringBoot3._Java24Maven.employee.service.controller;
 
 import com.rish889.rishSpringBoot3._Java24Maven.employee.service.dto.EmployeeDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/employee-service/v1/employees")
+@Slf4j
 public class EmployeeController {
 
     @GetMapping("/{id}")
     public @ResponseBody ResponseEntity<EmployeeDto> getEmployee(@PathVariable String id) {
+        log.info("getEmployee(). id : {}", id);
         return ResponseEntity.ok(new EmployeeDto(id, "John Doe"));
     }
 
